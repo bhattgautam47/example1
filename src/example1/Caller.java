e27911482b2e55f7ca2231b8d0b44b46a7d8166c
Gautam Bhatt <bhatt.gautam47@gmail.com>
2020-12-26 16:58:03 +0530

    second commit

---
+++ src/example1/Caller.java
@@ -0,0 +1,9 @@
+package example1;
+
+public class Caller {
+	public static void main(String[] args) {
+		Listeg l = new Listeg();
+		System.out.println(l.add(2,6));
+	}
+
+}
---
+++ src/example1/Listeg.java
@@ -0,0 +1,35 @@
+package example1;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Listeg {
+
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		LinkedList<String> ll = new LinkedList<String>();
+//		ll.push("Ravi");
+//		ll.push("Vijay");
+//		ll.push("Ajay");
+//		System.out.println("Size = "+ll.size());
+//		Listeg l = new Listeg();
+//		int output = l.add(1,4);
+//		System.out.println("output = "+output);
+//
+// }
+
+	 int add(int a, int b){
+		 int c = a+b;
+		 
+//		class B{
+//			void a() {
+//				System.out.println("Hi = ");
+//			}
+		//}
+		 return c;
+		 
+
+  }
+}
+
